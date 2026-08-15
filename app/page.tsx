47c141fc796cb6755c6b6b4cd358af2e2c4ed962
MdFaiz341 <faiz8969217468@gmail.com>
2026-08-16 01:37:17 +0530

    Prepare for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-// import MeshBackground from "@/components/MeshBackground";
+
 import Nav from "@/components/Navbar";
 import {Hero} from "@/components/Hero";
 import Stats from "@/components/Stats";
 import About from "@/components/About";
-// import Marquee from "@/components/Marquee";
 import Interests from "@/components/Interests";
 import FeaturedProject from "@/components/FeaturedProject";
 import Contact from "@/components/Contact";
@@ -12,12 +11,10 @@ import { Toolkit } from "@/components/Toolkit";
 export default function Home() {
   return (
     <main className="relative">
-      {/* <MeshBackground /> */}
       <Nav />
       <Hero />
       <Stats />
       <About />
-      {/* <Marquee /> */}
       <Toolkit/>
       <Interests />
       <FeaturedProject />
